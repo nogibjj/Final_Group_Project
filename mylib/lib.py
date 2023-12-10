75adb358f80a9c362cb75ee4adfe3f83c2b9642f
cassiekang <143849077+cassiekang@users.noreply.github.com>
2023-12-10 12:53:30 +0000

    update lib.py main.py

--- mylib/lib.py
+++ mylib/lib.py
@@ -1,61 +1,53 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
 from statistics import mean
 
-# This file includes all the functions created. 
-
 def plot_popularity(data):
-    # Create a figure with a custom size
     plt.figure(figsize=(12, 5))
-    # Set the seaborn theme to darkgrid
     sns.set_theme(style='darkgrid')
     sns.histplot(data["track_popularity"], kde=True)
-    # Add labels to the x-axis and y-axis
     plt.xlabel('Popularity', fontsize=14)
     plt.ylabel('Density', fontsize=14)
-    # Add a title to the plot
-    plt.title('Distribution of Track Popularity',fontsize=15)
+    plt.title('Distribution of Track Popularity', fontsize=15)
+    plt.savefig('popularity_distribution.png')
+    plt.close()
 
 def genre_count(data):
-    # Count the number of tracks in each genre
     counts = data['playlist_genre'].value_counts()
     top_genre = counts.reset_index()
     top_genre.columns = ['Playlist Genre', 'Track Counts']
     return top_genre
 
 def genre_popularity(data):
-    # Set the figure size
     plt.figure(figsize=(12, 8))
-    # Create the point plot
-    sns.pointplot(x = 'playlist_genre', y='track_popularity', data=data, estimator = np.mean)
-    # Add axis labels and a title
-    plt.xlabel('Playlist Genre',fontsize=14)
-    plt.ylabel('Average Popularity',fontsize=14)
-    plt.title('Average Popularity by Music Genre',fontsize=15)
+    sns.pointplot(x='playlist_genre', y='track_popularity', data=data, estimator=mean)
+    plt.xlabel('Playlist Genre', fontsize=14)
+    plt.ylabel('Average Popularity', fontsize=14)
+    plt.title('Average Popularity by Music Genre', fontsize=15)
+    plt.savefig('genre_popularity.png')
+    plt.close()
 
 def month_popularity(data):
-    data['track_album_release_date'] = data['track_album_release_date'].astype(str)
-    # Filter out rows with different date formats
-    data = data[pd.to_datetime(data['track_album_release_date'], errors='coerce').notna()]
-    # Convert the 'track_album_release_date' column to datetime
-    data['track_album_release_date'] = pd.to_datetime(data['track_album_release_date'])
-    # Extract the month from the datetime values
+    data = data.copy()
+    data['track_album_release_date'] = pd.to_datetime(data['track_album_release_date'], errors='coerce')
+    data.dropna(subset=['track_album_release_date'], inplace=True)
     data['release_month'] = data['track_album_release_date'].dt.month_name()
+
     month_order = [
         'January', 'February', 'March', 'April', 'May', 'June',
         'July', 'August', 'September', 'October', 'November', 'December'
     ]
+
     plt.figure(figsize=(12, 8))
-    sns.pointplot(x='release_month',  y='track_popularity', data=data, palette='viridis',estimator = np.mean,order=month_order)
-    # Set labels and title
+    sns.pointplot(x='release_month', y='track_popularity', data=data, estimator=mean, order=month_order)
     plt.xlabel('Release Month', fontsize=14)
     plt.ylabel('Average Track Popularity', fontsize=14)
     plt.title('Average Track Popularity by Month', fontsize=15)
+    plt.savefig('monthly_popularity.png')
+    plt.close()
 
 def find_highest_popularity_track(data):
-    """Find the track with the highest popularity score."""
     max_popularity = data['track_popularity'].max()
     most_popular_tracks = data[data['track_popularity'] == max_popularity]
     return most_popular_tracks[['track_name', 'track_artist', 'track_popularity']]
