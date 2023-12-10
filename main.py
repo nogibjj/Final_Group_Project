a5f2977d7b1ea30391a0e5a6e1450d255536977d
katherinetian540 <rui.tian@duke.edu>
2023-12-10 16:36:05 -0500

    add sql database

--- main.py
+++ main.py
@@ -1,8 +1,43 @@
 from mylib.lib import (plot_popularity,genre_count,genre_popularity,
                        month_popularity, find_highest_popularity_track)
 import pandas as pd
+import mysql.connector
+import os
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+
+
+def get_connection():
+    return mysql.connector.connect(
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME"),
+        port=8000,
+    )
+
+def save_data(connection, row):
+    cursor = connection.cursor()
+    track_id, *rest = row
+    query = "INSERT INTO spotify VALUES (%s, %s, %s, %s, %s, %s, %s,%s, %s, %s, %s, %s, %s, %s,%s, %s, %s, %s, %s, %s, %s,%s, %s, %s);"
+    cursor.execute(query)
+    connection.commit()
+
+def save_all_data():
+    df = pd.read_csv("./spotify_songs.csv", encoding="utf-8")
+    connection = get_connection()
+    for index, row in df.iterrows():
+        try:
+            save_data(connection, row)
+        except Exception as e:
+            print(e)
+            continue
+    connection.close()
 
 def main():
+    save_all_data()
     spotify = pd.read_csv("spotify_songs.csv")
     plot_popularity(spotify)
     genre_table = genre_count(spotify)
--- mylib/query.py
+++ mylib/query.py
@@ -1,199 +1,40 @@
-"""Query the database"""
+"""Database query lib"""
+import json
+# import MySQLdb
+import mysql.connector
+import os
+from dotenv import load_dotenv
 
-import sqlite3
+# Load environment variables from .env file
+load_dotenv()
 
-# Define a global variable for the log file
-LOG_FILE = "query_log.md"
-
-
-def log_query(query):
-    """adds to a query markdown file"""
-    with open(LOG_FILE, "a") as file:
-        file.write(f"```sql\n{query}\n```\n\n")
-
-
-def general_query(query):
-    """runs a query a user inputs"""
-    # Connect to the SQLite database
-    conn = sqlite3.connect("spotifyDB.db")
-
-    # Create a cursor object to execute SQL queries
-    cursor = conn.cursor()
-
-    # Execute the query
-    cursor.execute(query)
-
-    # If the query modifies the database, commit the changes
-    if (
-        query.strip().lower().startswith("insert")
-        or query.strip().lower().startswith("update")
-        or query.strip().lower().startswith("delete")
-    ):
-        conn.commit()
-
-    # Close the cursor and connection
-    cursor.close()
-    conn.close()
-
-    log_query(f"{query}")
-
-
-def create_record(
-   track_name,
-   artist(s)_name,
-   artist_count,
-   released_year,
-   released_month,
-   released_day,
-   in_spotify_playlists,
-   in_spotify_charts,
-   streams,
-   in_apple_playlists,
-):
-    """create example query"""
-    conn = sqlite3.connect("spotifyDB.db")
-    c = conn.cursor()
-    c.execute(
-        """
-        INSERT INTO spotifyDB
-        (track_name,
-         artist(s)_name,
-         artist_count,
-         released_year,
-         released_month,
-         released_day,
-         in_spotify_playlists,
-         in_spotify_charts,
-         streams,
-         in_apple_playlists
-         ) 
-         VALUES (?,?, ?, ?, ?,?,?,?,?,?)
-        """,
-        (
-         track_name,
-         artist(s)_name,
-         artist_count,
-         released_year,
-         released_month,
-         released_day,
-         in_spotify_playlists,
-         in_spotify_charts,
-         streams,
-         in_apple_playlists,
-        ),
-    )
-    conn.commit()
-    conn.close()
-
-    # Log the query
-    log_query(
-        f"""INSERT INTO spotifyDB VALUES (
-            {track_name}, 
-            {artist(s)_name},
-            {artist_count}, 
-            {released_year}, 
-            {released_month},
-            {released_day},
-            {in_spotify_playlists},
-            {in_spotify_charts},
-            {streams},
-            {in_apple_playlists});"""
-    )
-
-
-def update_record(
-   track_name,
-   artist(s)_name,
-   artist_count,
-   released_year,
-   released_month,
-   released_day,
-   in_spotify_playlists,
-   in_spotify_charts,
-   streams,
-   in_apple_playlists,
-):
-    """update example query"""
-    conn = sqlite3.connect("spotifyDB.db")
-    c = conn.cursor()
-    print(
-        (track_name,
-   artist(s)_name,
-   artist_count,
-   released_year,
-   released_month,
-   released_day,
-   in_spotify_playlists,
-   in_spotify_charts,
-   streams,
-   in_apple_playlists,
+class DB:
+    """DB utility class to connect and query ICU data"""
+    def __init__(self):
+        self.connection = mysql.connector.connect(
+            host=os.getenv("DB_HOST"),        # Use environment variable
+            user=os.getenv("DB_USER"),        # Use environment variable
+            passwd=os.getenv("DB_PASSWORD"),  # Use environment variable
+            db=os.getenv("DB_NAME"),          # Use environment variable
+            port=3306,    # Use environment variable (if needed)
         )
-    )
-    c.execute(
-        """
-        UPDATE spotifyDB 
-        SET track_name = ?,
-   artist(s)_name = ?,
-   artist_count = ?,
-   released_year = ?,
-   released_month = ?,
-   released_day = ?,
-   in_spotify_playlists = ?,
-   in_spotify_charts = ?,
-   streams = ?,
-   in_apple_playlists=?
-        WHERE id=?;
-        """,
-        (
-            track_name,
-   artist(s)_name,
-   artist_count,
-   released_year,
-   released_month,
-   released_day,
-   in_spotify_playlists,
-   in_spotify_charts,
-   streams,
-   in_apple_playlists,
-        ),
-    )
+        self.cursor = self.connection.cursor()
 
-    conn.commit()
-    conn.close()
+    def get_song_info(self, track_id):
+        """Return music info for a specified track"""
+        query = f'SELECT track_id, track_name, track_artist, danceability, energy FROM spotify_songs WHERE track_id = "{track_id}"'
+        self.cursor.execute(query)
+        data = self.cursor.fetchall()
 
-    # Log the query
-    log_query(
-        f"""UPDATE spotifyDB SET 
-        track_name={track_name}, 
-        artist(s)_name={artist(s)_name},
-        artist_count={artist_count},
-        released_year={released_year}, 
-        released_day={released_day},
-        in_spotify_playlists={in_spotify_playlists},
-        in_spotify_charts={in_spotify_charts},
-        streams={streams},
-        in_apple_playlists={in_apple_playlists}
-        WHERE id={record_id};"""
-    )
+        result = [{'track_id': row[0], 'danceability':row[1] ,'energy': row[2],} for row in data]
+        return json.dumps(result)
 
 
-def delete_record(record_id):
-    """delete example query"""
-    conn = sqlite3.connect("spotifyDB.db")
-    c = conn.cursor()
-    c.execute("DELETE FROM spotifyDB WHERE id=?", (record_id,))
-    conn.commit()
-    conn.close()
+    def get_popularity_info(self):
+        """Return summary info about track popularity"""
+        query = 'SELECT genre, COUNT(*) FROM spotify_songs GROUP BY genre'
+        self.cursor.execute(query)
+        data = self.cursor.fetchall()
 
-    # Log the query
-    log_query(f"DELETE FROM spotifyDB WHERE id={record_id};")
-
-
-def read_data():
-    """read data"""
-    conn = sqlite3.connect("spotifyDB.db")
-    c = conn.cursor()
-    c.execute("SELECT * FROM spotifyDB")
-    data = c.fetchall()
-    log_query("SELECT * FROM spotifyDB;")
-    return data
+        result = [{'genre': row[0], 'count': row[1]} for row in data]
+        return json.dumps(result)
